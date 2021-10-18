5c0fe15ab4c3e4a52f105f0fe00c916ff8926370
Julien Girard <jgirard@boole.ns42.fr>
2021-10-18 12:06:36 +0200

    Add python config file to be able to use history/completion

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,7 +5,8 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [[ -d ~/.tmux/plugins/tpm ]] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # 
-for i in .vimrc .bashrc .inputrc .tmux.conf .bash_profile
+LIST_OF_FILES=".vimrc .bashrc .inputrc .tmux.conf .bash_profile .pystartup"
+for i in $LIST_OF_FILES
 do
     [[ ~/$i -ef ${BASEDIR}/$i ]] || ln -vis ${BASEDIR}/$i ~/$i
 done
